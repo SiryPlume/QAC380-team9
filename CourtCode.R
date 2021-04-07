6a416f9da1d0af668573ab05df1e76359fbedd18
Lars Delin <ldelin@wesleyan.edu>
2021-04-07 17:26:26 -0400

    Renaming and eliminating unnecessary variables

--- CourtCode.R
+++ CourtCode.R
@@ -1,1 +1,14 @@
-# Hi guys. This is a test
+library(stringi)
+library(dplyr)
+block_text <- read_csv("~/Desktop/ /block_text.csv")
+
+CourtData <- block_text[c(1,7)]
+
+CourtData <- rename(CourtData, Document = original_file_name)
+CourtData <- rename(CourtData, Text = block_text)
+
+#CourtData$Date <- substring(CourtData$Document, 1, 6)
+#CourtData$State <- substring(CourtData$Document, 1, 2)
+
+#CourtData$Date <- as.numeric(CourtData$Date)
+#CourtData$State <- as.numeric(CourtData$Date)

f4e796b397f26083dc386204c5e8c6bb5fd5acfa
Lars Delin <ldelin@wesleyan.edu>
2021-04-14 09:23:36 -0400

    New Version

--- CourtCode.R
+++ CourtCode.R
@@ -1,14 +1,14 @@
-library(stringi)
+library(stringr)
 library(dplyr)
-block_text <- read_csv("~/Desktop/ /block_text.csv")
+block_text <- read_csv("~/Desktop/ /QAC380/block_text.csv")
 
 CourtData <- block_text[c(1,7)]
 
 CourtData <- rename(CourtData, Document = original_file_name)
 CourtData <- rename(CourtData, Text = block_text)
 
-#CourtData$Date <- substring(CourtData$Document, 1, 6)
-#CourtData$State <- substring(CourtData$Document, 1, 2)
+words <- c("COVID", "pandemic")
+temp <- strsplit(CourtData$Text[55], " ")[[1]]
+temp <- unique(tolower(temp))
 
-#CourtData$Date <- as.numeric(CourtData$Date)
-#CourtData$State <- as.numeric(CourtData$Date)
+str_detect(temp, "if")
